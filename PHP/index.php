c7f5a279e2cc5b27d9380fff3f241293346296f5
MannuGA <j.mannuelg.a@gmail.com>
2023-11-28 19:34:27 -0600

    a

---
+++ PHP/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$servername = "tu_servidor_mysql";
+$username = "tu_usuario_mysql";
+$password = "tu_contraseña_mysql";
+$dbname = "tu_base_de_datos";
+
+// Crear la conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Consulta SQL
+$sql = "SELECT id, nombre, email FROM usuarios";
+$result = $conn->query($sql);
+
+// Cerrar la conexión
+$conn->close();
+
+?>
